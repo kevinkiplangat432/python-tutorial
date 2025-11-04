6eaa88a6808aa16002ffd3d26ce97f883c91bf08
kevin kiplangat <kiplangatkevin335@gmail.com>
2025-11-04 18:20:31 +0300

    f strings

--- string_formatting_practice.py
+++ string_formatting_practice.py
@@ -1,13 +1,24 @@
 # String Formatting
 
-# Task 1:
-# Use f-strings to print a formatted message: "Welcome, Kevin! Your balance is 4500 KES."
+# Task 1: Using f-strings
+name = "Kevin"
+balance = 4500
+print(f"Task 1:\nWelcome, {name}! Your balance is {balance} KES.\n")
 
-# Task 2:
-# Use .format() and % formatting for the same message.
 
-# Task 3:
-# Align and format strings in a table-like structure.
+# Task 2: Using .format() and % formatting
+# Using .format()
+print("Task 2:")
+print("Welcome, {}! Your balance is {} KES.".format(name, balance))
 
-# Flask Prep Connection:
-# Useful for creating readable template strings or logging messages.
+# Using % formatting
+print("Welcome, %s! Your balance is %d KES.\n" % (name, balance))
+
+
+# Task 3: Align and format strings in a table-like structure
+print("Task 3:")
+print(f"{'Name':<10} {'Balance':>10}")
+print("-" * 22)
+print(f"{'Kevin':<10} {4500:>10}")
+print(f"{'Alice':<10} {12500:>10}")
+print(f"{'Brian':<10} {700:>10}")
